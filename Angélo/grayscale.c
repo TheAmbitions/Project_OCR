4c1f54b3755c7853eba7f77447ec22c8ac0f7c1c
Angelo EAP <angelo.eap@epita.fr>
2021-10-15 20:03:39 +0200

    otsu

--- Angélo/grayscale.c
+++ Angélo/grayscale.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <err.h>
+#include<math.h>
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 #include "pixel_operations.h"
@@ -70,74 +71,6 @@ void wait_for_keypressed()
 
 void SDL_FreeSurface(SDL_Surface *surface);
 
-void blur() //This manipulates with SDL_Surface and gives it box blur effect
-{
-   	SDL_Surface* imageSurface;
-
-	// Initialize the SDL
-	init_sdl();
-
-	imageSurface = load_image("image_05.jpeg");
-	int blur_extent = 1;	
-	
-	for (int y = 0; y < imageSurface->h; y++)
-	{
-		for (int x = 0; x < (imageSurface->pitch / 4); x++)
-		{
-		    Uint32 color = ((Uint32*)imageSurface->pixels)[(y * (imageSurface->pitch / 4)) + x];
-
-		    //SDL_GetRGBA() is a method for getting color
-		    //components from a 32 bit color
-		    Uint8 r = 0, g = 0, b = 0, a = 0;
-		    SDL_GetRGBA(color, imageSurface->format, &r, &g, &b, &a);
-
-		    Uint32 rb = 0, gb = 0, bb = 0, ab = 0;
-
-		    //Within the two for-loops below, colors of adjacent pixels are added up
-
-		    for (int yo = -blur_extent; yo <= blur_extent; yo++)
-		    {
-			for (int xo = -blur_extent; xo <= blur_extent; xo++)                 {                   if (y + yo >= 0 && x + xo >= 0
-				&& y + yo < imageSurface->h && x + xo < (imageSurface->pitch / 4)
-				)
-			    {
-				Uint32 colOth = ((Uint32*)imageSurface->pixels)[((y + yo)
-				                        * (imageSurface->pitch / 4)) + (x + xo)];
-
-				Uint8 ro = 0, go = 0, bo = 0, ao = 0;
-				SDL_GetRGBA(colOth, imageSurface->format, &ro, &go, &bo, &ao);
-
-				rb += ro;
-				gb += go;
-				bb += bo;
-				ab += ao;
-			    }
-			}
-		    }
-
-		    //The sum is then, divided by the total number of
-		    //pixels present in a block of blur radius
-
-		    //For blur_extent 1, it will be 9
-		    //For blur_extent 2, it will be 25
-		    //and so on...
-
-		    //In this way, we are getting the average of
-		    //all the pixels in a block of blur radius
-
-		    //(((blur_extent * 2) + 1) * ((blur_extent * 2) + 1)) calculates
-		    //the total number of pixels present in a block of blur radius
-
-		    r = (Uint8)(rb / (((blur_extent * 2) + 1) * ((blur_extent * 2) + 1)));
-		    g = (Uint8)(gb / (((blur_extent * 2) + 1) * ((blur_extent * 2) + 1)));
-		    b = (Uint8)(bb / (((blur_extent * 2) + 1) * ((blur_extent * 2) + 1)));
-		    a = (Uint8)(ab / (((blur_extent * 2) + 1) * ((blur_extent * 2) + 1)));
-
-		    //Bit shifting color bits to form a 32 bit proper colour
-		    color = (r) | (g << 8) | (b << 16) | (a << 24);           ((Uint32*)imageSurface->pixels)[(y * (imageSurface->pitch / 4)) + x] = color;
-		}
-	}
-}
 
 SDL_Surface* Filter(SDL_Surface* source)
 {
@@ -153,36 +86,73 @@ SDL_Surface* Filter(SDL_Surface* source)
         	target = SDL_CreateRGBSurface( SDL_SWSURFACE, source->w ,source->h, source->format->BitsPerPixel, source->format->Rmask, source->format->Gmask, source->format->Bmask, source->format->Amask );
 	}
 
-	for(y=1; y<source->h; ++y)
+	int w = source->w;
+	int h = source->h;
+	for(y=0; y<h; y++)
 	{
-		for(x=1; x<source->w; ++x)
+		for(x=0; x<w; x++)
 		{
 			unsigned r = 0, g = 0, b = 0;
 			Uint8 rt, gt, bt;
+			if (y != 0) 
+			{
+			   
+			   	unsigned o = get_pixel(source, x, y-1);
+				SDL_GetRGB(o, source->format, &rt, &gt, &bt);
+				r += rt;
+				g += gt;
+				b += bt;
+				
+			}
+			
 
-			unsigned a = get_pixel(source,x-1, y-1);
-			SDL_GetRGB(a, source->format, &rt, &gt, &bt);
-			r += rt;
-			g += gt;
-			b += bt;
-		   
-		   	unsigned o = get_pixel(source, x, y-1);
-			SDL_GetRGB(o, source->format, &rt, &gt, &bt);
-			r += rt;
-			g += gt;
-			b += bt;
+			if (x != 0)
+			{
+				unsigned d = get_pixel(source, x-1, y);
+				SDL_GetRGB(d, source->format, &rt, &gt, &bt);
+				r += rt;
+				g += gt;
+				b += bt;
+			}
 			
-			unsigned c = get_pixel(source, x+1, y-1);
-			SDL_GetRGB(c, source->format, &rt, &gt, &bt);
-			r += rt;
-			g += gt;
-			b += bt;
+			if ((x != 0) && (y != 0))
+			{
+				unsigned a = get_pixel(source,x-1, y-1);
+				SDL_GetRGB(a, source->format, &rt, &gt, &bt);
+				r += rt;
+				g += gt;
+				b += bt;
+			}
 			
-			unsigned d = get_pixel(source, x-1, y);
-			SDL_GetRGB(d, source->format, &rt, &gt, &bt);
-			r += rt;
-			g += gt;
-			b += bt;
+			if ((x < w-1) && (y !=0))
+			{
+				
+				unsigned c = get_pixel(source, x+1, y-1);
+				SDL_GetRGB(c, source->format, &rt, &gt, &bt);
+				r += rt;
+				g += gt;
+				b += bt;
+			}
+			
+			if (x < w-1) 
+			{
+				unsigned e = get_pixel(source,x+1, y);
+				SDL_GetRGB(e, source->format, &rt, &gt, &bt);
+				r += rt;
+				g += gt;
+				b += bt;
+			}
+			
+			if ((x !=0 ) && (y < h+1))
+			{
+					
+				unsigned f = get_pixel(source,x-1, y+1);
+				SDL_GetRGB(f, source->format, &rt, &gt, &bt);
+				r += rt;
+				g += gt;
+				b += bt;
+			
+			}
 			
 			
 			unsigned z = get_pixel(source, x, y);
@@ -192,33 +162,28 @@ SDL_Surface* Filter(SDL_Surface* source)
 			b += bt;
 			
 			
+			if (y < h-1) 
+			{
+				unsigned k = get_pixel(source,x  , y+1);
+				SDL_GetRGB(k, source->format, &rt, &gt, &bt);
+				r += rt;
+				g += gt;
+				b += bt;
+			}
 			
-			unsigned e = get_pixel(source,x+1, y);
-			SDL_GetRGB(e, source->format, &rt, &gt, &bt);
-			r += rt;
-			g += gt;
-			b += bt;
-			
-			
-			unsigned f = get_pixel(source,x-1, y+1);
-			SDL_GetRGB(f, source->format, &rt, &gt, &bt);
-			r += rt;
-			g += gt;
-			b += bt;
-			
-			
-			unsigned k = get_pixel(source,x  , y+1);
-			SDL_GetRGB(k, source->format, &rt, &gt, &bt);
-			r += rt;
-			g += gt;
-			b += bt;
-			
-			
-			unsigned h = get_pixel(source,x+1, y+1);
-			SDL_GetRGB(h, source->format, &rt, &gt, &bt);
-			r += rt;
-			g += gt;
-			b += bt;
+						
+			if ((x < w+1 ) && (y < h+1))
+			{
+					
+				unsigned h = get_pixel(source,x+1, y+1);
+				SDL_GetRGB(h, source->format, &rt, &gt, &bt);
+				r += rt;
+				g += gt;
+				b += bt;
+							
+			}
+		
+
 		   
 		   	r/=9;
 		   	g/=9;
@@ -231,12 +196,175 @@ SDL_Surface* Filter(SDL_Surface* source)
 	return target;
 }
 
+float compute_first_order_cumulative_moment(float* hist,int k)
+{
+	float first_order_cumulative_moment = 0;
+
+	for (int i = 0; i < k; i++)
+
+	{
+
+		first_order_cumulative_moment += i*hist[i];
+
+	}
+
+
+	return first_order_cumulative_moment;
+}
+
+float compute_zero_order_cumulative_moment(float* hist,int k)
+{
+	float zero_order_cumulative_moment = 0;
+
+	for (int i = 0; i < k; i++)
+
+	{
+
+		zero_order_cumulative_moment += i*hist[i];
+
+	}
+
+
+	return zero_order_cumulative_moment;
+}
+
+float compute_variance_class_separability(float uT,float wk, float uk)
+
+{
+
+    return pow((uT*wk-uk),2)/(wk*(1-wk));
+
+}
+
+void otsu(SDL_Surface* img, float seuil)
+
+{
+
+	float hist[256];
+
+	for (int i = 0; i < 256; i++)
+
+	{
+
+		hist[i] = 0;
+
+	}
+
+	int width = img -> w;
+	int height = img -> h;
+	Uint8 r,g,b;
+	for (int i = 0; i < height ; i++)
+
+	{
+
+		for (int j = 0; j < width; j++)
+
+		{
+			Uint32 pixel = get_pixel(img, j, i);
+			SDL_GetRGB(pixel, img->format, &r , &g, &b);
+			hist[r] += 1;
+
+		}
+
+	}
+
+	int N = width*height;
+
+	for (int i = 0; i < 256; i++)
+
+	{
+
+		hist[i] = hist[i] / N;
+
+	}
+
+	float w[256],u[256],uT;
+
+	for (int i = 0; i < 256; i++)
+
+	{
+
+		w[i] = compute_zero_order_cumulative_moment(hist, i);
+
+		u[i] = compute_first_order_cumulative_moment(hist, i);
+
+	}
+
+	uT = compute_first_order_cumulative_moment(hist, 256);
+
+	float variance_class_separability_max = -1;
+
+	float best_threesold = 0;
+
+	for (int i = 0; i < 256; i++)
+
+	{
+
+		int vk = compute_variance_class_separability(uT, w[i], u[i]);
+
+		if (vk > variance_class_separability_max)
+
+		{
+
+			variance_class_separability_max = vk;
+
+			best_threesold = i;
+
+		}
+
+	}
+
+	for (int i = 0; i < height; i++)
+
+	{
+
+		for (int j = 0; j < width; j++)
+
+		{
+
+			Uint32 pixel = get_pixel(img, j, i);
+			SDL_GetRGB(pixel, img->format, &r ,&g,&b);
+			if (r < best_threesold * seuil)
+
+			{
+
+				pixel = SDL_MapRGB(img ->format, 0, 0, 0);
+				put_pixel(img,j,i,pixel);
+
+			}
+
+			else
+
+			{
+
+				pixel = SDL_MapRGB(img->format, 255, 255, 255);
+				put_pixel(img,j,i,pixel);
+
+			}
+
+		}
+
+	}
+
+}
+
 int main(int argc,char *argv[])
 {
-	char* filename;
+	char *filename;
 	if (argc < 2)
 		errx(1, "must provide filename");
 	filename = argv[1];
+	
+	float seuil;
+	printf("enter threshold \n");
+	
+	if (scanf("%f", &seuil) < 0) {
+	    printf("you want a positive seuil.\n");
+	    return 1;
+	} 
+	
+	
+	
 	
 	SDL_Surface* image_surface;
 	SDL_Surface* screen_surface;
@@ -251,17 +379,20 @@ int main(int argc,char *argv[])
 	// Wait for a key to be pressed.
 	wait_for_keypressed();
 	
+	
 	Uint32 pixel;
 	int w;
 	int h;
 	w = image_surface -> w;
 	h = image_surface -> h;
+	
+	long int total_pixels = w*h;
+	printf("%li",total_pixels);
 	Uint8 r, g, b, average;
  
-    	image_surface = Filter(image_surface);
-    	update_surface(screen_surface, image_surface);
-    	wait_for_keypressed();
- 
+    	
+
+    
 	for(int i = 0; i < w; i++)
 	{
 		for(int j = 0; j < h; j++)
@@ -269,44 +400,24 @@ int main(int argc,char *argv[])
 			pixel = get_pixel(image_surface, i, j);
 			SDL_GetRGB(pixel, image_surface->format, &r, &g, &b);
 			average = r * 0.3 + g * 0.59 + b * 0.11;
-			/*if (average < 128 && average > 0 )
-        		{
-		            pixel = SDL_MapRGB(image_surface->format, average, average, average);
-		        }
-		        else if (average >= 128 && average< 255)
-		        {
-		            contraste = average + 2;
-		            pixel = SDL_MapRGB(image_surface->format, contraste, contraste, contraste);
-		        }*/
 		        
 			pixel = SDL_MapRGB(image_surface->format, average, average, average);
 			put_pixel(image_surface,i,j,pixel);
 		}
   	}
-
-	/*update_surface(screen_surface, image_surface);
+ 
+	image_surface = Filter(image_surface);
+	image_surface = Filter(image_surface);
 	
-	wait_for_keypressed();*/
 	
-	for(int i = 0; i < w; i++)
-	{
-		for(int j = 0; j < h; j++)
-		{
-			pixel = get_pixel(image_surface, i, j);
-			SDL_GetRGB(pixel, image_surface->format, &r, &g, &b);
-			
-			if(r < 127 && g < 127 && b < 127) 
-     			{
-		      		pixel = SDL_MapRGB(image_surface->format, 0, 0, 0);
-		     	}
-		     	else
-		     	{
-		     	 	pixel = SDL_MapRGB(image_surface->format, 255, 255, 255);
-		     	}
-		     	
-			put_pixel(image_surface,i,j,pixel);
-		}
-  	}
+	
+	update_surface(screen_surface, image_surface);
+	//update_surface(screen_surface, image_surface);
+	
+	wait_for_keypressed();
+	
+	
+	otsu(image_surface,seuil);
 	
 	
 	update_surface(screen_surface, image_surface);
