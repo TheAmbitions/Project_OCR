6fdb06399346dcd475653b960354658b17797d4b
Jules LEROQUAIS <jules.leroquais@epita.fr>
2021-10-21 17:25:01 +0200

    rotation main exterieur

--- Jules/Rotation/rotations.h
+++ Jules/Rotation/rotations.h
@@ -1,8 +1,12 @@
-
-
 #ifndef ROTATION_H
 #define ROTATION_H
 
-SDL_Surface* SDL_RotationCentralN(SDL_Surface* origine, float angle);
+#include <err.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "pixel_operations.h"
+#include <math.h>
+
+int apply_rotation();
 
 #endif
